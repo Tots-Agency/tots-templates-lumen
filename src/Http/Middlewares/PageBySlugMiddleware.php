adfcffba6db1e7af1ca96781ad35ceebd78346bc
Matias Camiletti <matias.camiletti@gmail.com>
2023-05-26 11:44:50 -0300

    Add page by slug middleware

---
+++ src/Http/Middlewares/PageBySlugMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tots\Templates\Http\Middlewares;
+
+use Closure;
+use Tots\Templates\Models\TotsPage;
+
+class PageBySlugMiddleware
+{
+    /**
+     * Obtiene la cuenta
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $pageId = $request->input('page_id') ?? $request->input('id');
+        if($pageId <= 0){
+            throw new \Exception('The page id is required');
+        }
+
+        $pageSlug = $request->input('slug');
+        if($pageSlug == ''){
+            throw new \Exception('The page slug is required');
+        }
+
+        $page = TotsPage::where('id', $pageId)->where('slug', $pageSlug)->first();
+        if($page === null){
+            throw new \Exception('The page not exist');
+        }
+
+        return $next($request->merge([TotsPage::class => $page]));
+    }
+}
