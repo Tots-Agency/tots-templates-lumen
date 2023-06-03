2f1e4600aaf63c9440ae10d505e25af3dc774b72
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-03 14:39:50 -0300

    Add custom view component.

---
+++ src/Views/TotsCustomViewComponent.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tots\Templates\Views;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Component;
+use Tots\Templates\Models\TotsComponent;
+
+class TotsCustomViewComponent extends Component
+{
+    protected TotsComponent $component;
+    protected $params = [];
+
+    /**
+     * Create the component instance.
+     */
+    public function __construct(TotsComponent $comp, $params)
+    {
+        $this->component = $comp;
+        $this->params = $params;
+    }
+
+    public function render()
+    {
+        //$html = '<h1>Prueba de primera vista</h1><p>Resultado: {{ customFunction(5, 10) }}</p>';
+        return Blade::render($this->component->html, ['name' => 'Julian Bashir', 'customFunction' => 'myCustomFunction']);
+    }
+
+    public function myCustomFunction($param1, $param2)
+    {
+        // Lógica de la función
+        // Puedes realizar cualquier procesamiento necesario
+        return $param1 + $param2;
+    }
+}
